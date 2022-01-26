c791ff55d1a40142c49ef724828271f82a6552e2
AdithyaSathish1 <89308385+AdithyaSathish1@users.noreply.github.com>
2022-01-26 12:37:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var s1
+var s2
+function setup() {
+  createCanvas(400,400);
+
+  s1=new Student("Adithya",13,33) 
+  s2=new Student("Rohan",7,32)
+  s1.display()
+  s2.display()
+
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,19 @@
+class Student{
+
+  constructor(name,age,roll_no) 
+  {
+  this.name=name;
+  this.age=age;
+  this.roll_no=roll_no;}
+
+
+display(){
+console.log(this.name)
+console.log(this.age)
+console.log(this.roll_no)
+
+}}
+
+
+
+
